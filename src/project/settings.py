b58066f7848666459fa55e4c2426634af9bbd93f
polinabychinskaya <bychinskaya008@gmail.com>
2023-05-21 13:06:45 +0300

    settings

--- src/project/settings.py
+++ src/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^ci(!7v9796b7&+66$i176ueh7@^du3z+g4&=+uqj4zq=4bcqr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['polinabychinskaya.pythonanywhere.com']
 
 
 # Application definition
@@ -116,7 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = 'home/polinabychinskaya/static'
+MEDIA_ROOT = '/home/polinabychinskaya/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
