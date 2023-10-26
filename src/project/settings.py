536941900cb3600c20ffdc3572413d06593fef46
polinabychinskaya <bychinskaya008@gmail.com>
2023-10-26 19:27:08 +0300

    new

--- src/project/settings.py
+++ src/project/settings.py
@@ -87,8 +87,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rizz',
+        'USER': 'admin',
+        'PASSWORD': 'polina2004',
     }
 }
 
